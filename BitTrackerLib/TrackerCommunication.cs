6d89ab379ac431739afb13ae2f0ceae57adecc97
cathcart <thomas.cathcart@gmail.com>
2021-12-05 11:00:58 +0000

    gets list of peers from Tracker

--- BitTrackerLib/TrackerCommunication.cs
+++ BitTrackerLib/TrackerCommunication.cs
@@ -1,10 +1,112 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+using BencodeLibRedo.Interfaces;
+using BitTrackerLib.Classes;
+using BitTrackerLib.Interfaces;
+
 namespace BitTrackerLib
 {
     public class TrackerCommunication
     {
-        public TrackerCommunication()
+        private string _announceUrl;
+        private byte[] _MetaInfoHash;
+        private BencodeParser _parser = new BencodeParser();
+        private byte[] _peer_id;
+        private DateTime _nextTrackerCommunication = DateTime.Now;
+
+        public TrackerCommunication() { }
+
+        public TrackerCommunication(string filepath)
         {
+            var bytes = ParseMetaInfo(filepath);
+
+            using (SHA1 sha = SHA1.Create())
+            {
+                _MetaInfoHash = sha.ComputeHash(bytes);
+
+                DateTime now = DateTime.Now;
+
+                string hostName = Dns.GetHostName(); // Retrive the Name of HOST
+                //string myIP = Dns.GetHostByName(hostName).AddressList[0].ToString(); // Get the IP
+                string myIP = Dns.GetHostEntry(hostName).AddressList[0].ToString();
+
+                var peerSeed = Encoding.UTF8.GetBytes(string.Format("{0}{1}", now.ToString(), myIP));
+                _peer_id = sha.ComputeHash(peerSeed);
+            }
+        }
+
+        private byte[] ParseMetaInfo(string filepath)
+        {
+            FileStream fs = File.OpenRead(filepath);
+
+            var item = _parser.Parse(fs);
+
+            Dictionary<string, IBencodeItem> metaInfo = item.Export();
+
+            IBencodeItem info = metaInfo["info"];
+
+            int len = info.StopPos - info.StartPos;
+            var bytes = new byte[len];
+
+            fs.Position = info.StartPos;
+            fs.Read(bytes, 0, len);
+
+            fs.Close();
+
+            _announceUrl = metaInfo["announce"].Export();
+
+            return bytes;
+        }
+
+        public IEnumerable<IPeer> GetPeers()
+        {
+            WebClient webClient = new WebClient();
+
+            webClient.QueryString.Add("info_hash", HttpUtility.UrlEncode(_MetaInfoHash));
+            webClient.QueryString.Add("peer_id", HttpUtility.UrlEncode(_peer_id));
+            string result = webClient.DownloadString(_announceUrl);
+
+            _parser = new BencodeParser();
+
+            var item = _parser.Parse(result);
+
+            Dictionary<string, IBencodeItem> trackerResponse = item.Export();
+
+            return ParseTrackerResponse(trackerResponse);
+        }
+
+        public IEnumerable<IPeer> ParseTrackerResponse(Dictionary<string, IBencodeItem> trackerResponse)
+        {
+            if (trackerResponse.ContainsKey("failure reason"))
+            {
+                string issue = trackerResponse["failure reason"].Export();
+
+                throw new Exception(issue);
+            }
+
+            var interval = trackerResponse["interval"].Export();
+
+            _nextTrackerCommunication = DateTime.Now.AddSeconds(interval);
+
+            var peers = trackerResponse["peers"].Export();
+
+            var returnList = new List<IPeer>();
+            foreach (IBencodeItem peer in peers)
+            {
+                //don't think we have to care about compact/non-compact representations here
+                string ip = peer.Export()["ip"].Export();
+                int port = peer.Export()["port"].Export();
+
+                var r_peer = new Peer(ip, port);
+                returnList.Add(r_peer);
+            }
+            return returnList;
         }
     }
 }
--- TrackerTests/parseMetadattest.cs
+++ TrackerTests/parseMetadattest.cs
@@ -6,60 +6,108 @@ using System.Security.Cryptography;
 using System.Text;
 using System.Web;
 using BencodeLibRedo.Interfaces;
-using BencodeLibRedo.Models;
+using BitTrackerLib;
+using BitTrackerLib.Interfaces;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace TestProject
 {
+    public static class TrackerCommunicationTestingExtension
+    {
+        public static IEnumerable<IPeer> GetPeersTest(this TrackerCommunication trackerCommunication, Dictionary<string,string> testTrackerResponse)
+        {
+
+            var encoder = new BencodeEncoder();
+            var parser = new BencodeParser();
+
+            var bencodedResponse = encoder.Encode(testTrackerResponse);
+
+
+            var ttmp = parser.Parse(bencodedResponse);
+            var tmp2 = ttmp.Export();
+
+            //Dictionary<string, IBencodeItem> tmp = (Dictionary<string, IBencodeItem>)parser.Parse(bencodedResponse);
+
+            return trackerCommunication.ParseTrackerResponse(tmp2);
+        }
+
+        public static IEnumerable<IPeer> GetPeersTest(this TrackerCommunication trackerCommunication, Dictionary<string, IBencodeItem> testTrackerResponse)
+        {
+            return trackerCommunication.ParseTrackerResponse(testTrackerResponse);
+        }
+    }
+
     [TestClass]
     public class MetaDataRun
     {
-        
-
         [TestMethod]
-        public void TestFileRead()
+        public void AssertTrackerFailed()
         {
-            var parser = new BencodeParser();
+            var failedResponse = new Dictionary<string,string> { { "failure reason", "faked response" } };
 
+            var tmp = new TrackerCommunication();
 
-            string filepath = "/Users/cathcart/Downloads/debian-10.0.0-amd64-DVD-1.iso.torrent";
-            filepath = "/Users/cathcart/Downloads/debian-live-11.1.0-amd64-cinnamon.iso.torrent";
-            //var file = new FileStream(filepath);
-
-            FileStream fs = File.OpenRead(filepath);
-
-            var item = parser.Parse(fs);
-            //ßßvar output = item.Export();
-            //var output = 32;
-
-            Dictionary<string, IBencodeItem> tmp = item.Export();
-
-            var ann = tmp["announce"].Export();
-
-
-
-            IBencodeItem info = tmp["info"];
-
-            int len = info.StopPos - info.StartPos;
-            var bytes = new byte[len];
-
-            fs.Position = info.StartPos;
-            fs.Read(bytes, 0, len);
-
-
-            var infotext = Encoding.UTF8.GetString(bytes);
-
-            var sha = SHA1.Create();
-
-            var hash = sha.ComputeHash(bytes);
-
-            var text = HttpUtility.UrlEncode(hash);
-
-
-            Assert.AreEqual<int>(1, 1);
-
-            
+            Assert.ThrowsException<Exception>(() => tmp.GetPeersTest(failedResponse));
         }
 
+        [TestMethod]
+        public void OnePeerResponse()
+        {
+            var encoder = new BencodeEncoder();
+            var parser = new BencodeParser();
+
+            var peer = new Dictionary<string, string> { { "ip", "192.168.1.1"}, {"port", "42"} };
+
+            string peerBenCode = encoder.Encode(peer);
+
+            var testResponse = new Dictionary<string, string> { { "8:interval", "i666e" }, { "5:peers", peerBenCode } };
+
+            var testResponseStr = string.Format("d8:intervali666e5:peersl{0}ee", peerBenCode);
+
+            Dictionary<string, IBencodeItem> tmp2 = parser.Parse(testResponseStr).Export();
+
+            var tmp = new TrackerCommunication();
+
+            IEnumerable<IPeer> peerList = tmp.GetPeersTest(tmp2);
+
+            Assert.AreEqual(1, peerList.Count());
+        }
+
+        [TestMethod]
+        public void TwoPeerResponse()
+        {
+            var encoder = new BencodeEncoder();
+            var parser = new BencodeParser();
+
+            var peer1 = new Dictionary<string, string> { { "ip", "192.168.1.1" }, { "port", "42" } };
+            var peer2 = new Dictionary<string, string> { { "ip", "192.168.1.2" }, { "port", "42" } };
+
+            string peer1BenCode = encoder.Encode(peer1);
+            string peer2BenCode = encoder.Encode(peer2);
+
+            //List<string> peers = new List<string> { peer1BenCode, peer2BenCode };
+            //string peerBenCode = encoder.Encode(peers);
+
+            //Dictionary<string, string> testResponse = new Dictionary<string, string> { { "8:interval", "i666e" }, { "5:peers", peerBenCode } };
+
+            var testResponseStr = string.Format("d8:intervali666e5:peersl{0}{1}ee", peer1BenCode, peer2BenCode);
+
+            Dictionary<string, IBencodeItem> tmp2 = parser.Parse(testResponseStr).Export();
+
+            var tmp = new TrackerCommunication();
+
+            IEnumerable<IPeer> peerList = tmp.GetPeersTest(tmp2);
+
+            Assert.AreEqual(2, peerList.Count());
+        }
+
+        [TestMethod]
+        public void GetPeersFromTracker()
+        {
+            string filepath = "/Users/cathcart/Downloads/debian-live-11.1.0-amd64-cinnamon.iso.torrent";
+            var tmp = new TrackerCommunication(filepath);
+
+            var peers = tmp.GetPeers();
+        }
     }
 }
